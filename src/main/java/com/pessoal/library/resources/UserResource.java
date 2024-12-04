29c0ab42d44aada33bdbe9b918cbd76516a2f97f
Vinícius <viniciusmoraesdasilva25@gmail.com>
2024-12-04 19:11:17 -0300

    implementation User CRUD methods

--- src/main/java/com/pessoal/library/resources/UserResource.java
+++ src/main/java/com/pessoal/library/resources/UserResource.java
@@ -1,9 +1,20 @@
 package com.pessoal.library.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pessoal.library.entities.User;
 import com.pessoal.library.services.UserService;
 
 @RestController
@@ -12,5 +23,34 @@ public class UserResource {
 	
 	@Autowired
 	private UserService userService;
-
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User userRequested = userService.findById(id);
+		return ResponseEntity.ok().body(userRequested);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		User savedUser = userService.insert(obj);
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@RequestBody User userData, @PathVariable Long id){
+		User updatedUser = userService.update(id, userData);
+		return ResponseEntity.ok(updatedUser);
+	}
 }
--- src/main/java/com/pessoal/library/services/UserService.java
+++ src/main/java/com/pessoal/library/services/UserService.java
@@ -1,13 +1,45 @@
 package com.pessoal.library.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pessoal.library.entities.User;
 import com.pessoal.library.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
+
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new RuntimeException("Object not found"));
+	}
+
+	public User insert(User obj) {
+		return userRepository.save(obj);
+	}
+
+	public void delete(Long id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+
+	public User update(Long id, User userData) {
+		User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Object not found"));
+		
+		user.setName(userData.getName());
+        user.setEmail(userData.getEmail());
+        user.setType(userData.getType());
+		
+        return userRepository.save(user);
+	}
 }
